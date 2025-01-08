7fc0ea56b5179e1018051446143166407bf8b039
jordano <jordano.fs.vt@gmail.com>
2025-01-08 15:06:52 -0300

    Add: menu function & leitura_tecla function

--- src/main.asm
+++ src/main.asm
@@ -31,15 +31,173 @@ segment code
     MOV [modo_anterior], AL   
 
 ;Alterar modo de video para grafico 640x480 16 cores
-    MOV AL, 12h
-   	MOV AH, 0
-    INT 10h
-
+    MOV 	AL, 12h
+   	MOV 	AH, 0
+    INT 	10h
+	
 ;Gera menu
+	MOV	byte [cor], branco_intenso
+	CALL MENU
+	MOV DL, 14 ;salva informaçoes para printar a seta de seleçao
+	MOV DH, 15
+	MOV	BX, 0
+
+SELECAO:
+		MOV		byte[cor], branco_intenso
+		CALL 	cursor
+    	MOV  	AL, [BX+selecao]
+		CALL 	caracter		
+    	MOV		word[tecla_primida], 0 
+		
+		CALL LEITURA_TECLA
+
+		CMP		byte[tecla_primida], 4Dh						;seta para a direita foi apertada
+		JE		TROCA_DIREITA
+		CMP		byte[tecla_primida], 4Bh						; seta para a esquerda foi apertada
+		JE		TROCA_ESQUERDA
+		CMP		byte[tecla_primida], 1Ch						; enter foi apertado
+		JE		JOGO
+		JMP 	SELECAO
+
+TROCA_DIREITA:
+		MOV		byte[cor], preto			;apaga seta		
+		CALL	cursor
+   	    MOV     AL, [BX+selecao]
+		CALL	caracter
+		CMP		DL, 53						; compara para saber se esta na ultima dificuldade selecionavel
+		JG		volta_dir
+		ADD		DL, 20						; pula pra proxima dificuldade
+		JMP		SELECAO
+
+volta_dir:									; da a volta pela direita para a primeira dificuldade
+		MOV		DL, 14
+		JMP		SELECAO
+
+TROCA_ESQUERDA:
+		MOV	byte[cor], preto			;apaga seta		
+		CALL cursor
+    	MOV AL, [BX+selecao]
+		CALL	caracter
+
+		CMP		DL, 20						; compara para saber se esta na primeira dificuldade selecionavel
+		JL		volta_esq
+		SUB		DL, 20						; pula pra dificuldade anterior
+		JMP		SELECAO
+
+volta_esq:	
+		MOV		DL, 54						; da a volta pela direita para a ultima dificuldade
+		JMP		SELECAO
+
+;inicia o jogo, a dificuldade depende do valor de DL
+JOGO:	
+		;apaga seta
+		MOV		byte[cor], preto					
+		CALL	cursor
+   	    MOV     AL, [BX+selecao]
+		CALL	caracter
+		;apaga menu	
+		CALL	MENU
+		;cria linha delimitadora inferior 
+		MOV		byte[cor], branco_intenso					
+		XOR 	AX, AX
+		MOV 	AX, 40
+		PUSH 	AX
+		MOV 	AX, 40
+		PUSH 	AX
+		MOV 	AX, 600
+		PUSH 	AX
+		MOV 	AX, 40
+		PUSH 	AX
+		CALL 	line
+
+		;cria linha delimitadora superior
+		MOV 	AX, 40
+		PUSH 	AX
+		MOV 	AX, 440
+		PUSH 	AX
+		MOV 	AX, 600
+		PUSH 	AX
+		MOV 	AX, 440
+		PUSH 	AX
+		CALL 	line
+
+    ;desenha os blocos dos jogadores
+    	CALL 	DESENHA_BLOCOS_P1_E_P2
+
+MOVIMENTOS:
+		CALL	LEITURA_TECLA 
+		;verifica se foi pausado
+		CMP		byte[tecla_primida], 19h
+		JE		PAUSE
+		;verifica se a tecla de saida foi primida e sai caso sim
+		CMP 	byte[tecla_primida], 10h
+		JE 		FIM
+		JMP		MOVIMENTOS
+FIM:
+;sai do modo de video
+		CLI								
+        XOR     AX, AX					
+        MOV     ES, AX					
+        MOV     AX, [save_segment]			
+        MOV     [ES:INTr*4+2], AX		
+        MOV     AX, [save_offset]
+        MOV     [ES:INTr*4], AX
+		STI
+	 	MOV AH, 0   						; set video mode
+	 	MOV AL, [modo_anterior]   		; modo anterior
+	  	INT 10h
+		MOV AX, 4c00h
+		INT 21h
+		
+PAUSE:
+		CALL LEITURA_TECLA
+		CMP		byte[tecla_primida], 19h
+		JE		MOVIMENTOS
+		JMP		PAUSE
+
+INTERRUPCAO_TECLADO:
+		PUSHF
+		PUSH	AX	
+		PUSH	BX	
+		PUSH	CX	
+		PUSH	DX	
+		PUSH	DS
+		PUSH	ES	
+
+		IN 		AL, 0x60
+		INC     WORD [verifica1]
+		AND     WORD [verifica1], 7
+		;MOV		BX, [verifica1]
+		MOV		[1+tecla], AL
+
+		; Limpa a interrupção do teclado
+		IN      AL, kb_ctl				
+        OR      AL, 80h					
+        OUT     kb_ctl, AL				
+        AND     AL, 7Fh					
+        OUT     kb_ctl, AL	
+		
+		; notifica fim da interrupçao ao PIC			
+        MOV     AL, eoi					
+        OUT     pictrl, AL
+		
+		POP		ES
+		POP		DS
+		POP 	DX
+		POP 	CX
+		POP 	BX
+		POP 	AX
+		POPF
+		IRET
+
 MENU:
+		PUSH DX
+		PUSH CX
+		PUSH BX
+		PUSH AX
+
 		MOV CX, 30						;número de caracteres
 		MOV BX, 0
-		MOV	byte [cor], branco_intenso
 		MOV DH, 25						;linha 0-29
     MOV DL, 24						;coluna 0-79s
 
@@ -91,180 +249,37 @@ DIFICIL:
     	INC	BX ;proximo caracter
 		INC	DL ;avanca a coluna
     	LOOP DIFICIL
-		MOV DL, 14 ;salva informaçoes para printar a seta de seleçao
-		MOV	BX, 0
-
-SELECAO:
-		MOV		byte[cor], branco_intenso
-		CALL 	cursor
-    	MOV  	AL, [BX+selecao]
-		CALL 	caracter		
-    	MOV		word[tecla_primida], 0 
 		
-TESTE1:	MOV		AX, [verifica1]
+		POP AX
+		POP BX
+		POP CX
+		POP DX
+
+		RET
+
+LEITURA_TECLA:
+		PUSHF
+		PUSH AX
+		PUSH BX
+		PUSH CX
+		PUSH DX
+		
+ESPERA:	MOV		AX, [verifica1]	
 		CMP		AX, [verifica2]
-		JE TESTE1
-		INC     WORD [verifica2]
-		AND     WORD [verifica2],7
-		XOR		AX, AX
-		MOV		AL, [1+tecla]
-		MOV 	[tecla_primida], AL
-
-		CMP		byte[tecla_primida], 4Dh						;seta para a direita foi apertada
-		JE		TROCA_DIREITA
-		CMP		byte[tecla_primida], 4Bh						; seta para a esquerda foi apertada
-		JE		TROCA_ESQUERDA
-		CMP		byte[tecla_primida], 1Ch						; enter foi apertado
-		JE		JOGO
-		JMP 	SELECAO
-
-TROCA_DIREITA:
-		MOV		byte[cor], preto			;apaga seta		
-		CALL	cursor
-   	    MOV     AL, [BX+selecao]
-		CALL	caracter
-		CMP		DL, 53						; compara para saber se esta na ultima dificuldade selecionavel
-		JG		volta_dir
-		ADD		DL, 20						; pula pra proxima dificuldade
-		JMP		SELECAO
-
-volta_dir:									; da a volta pela direita para a primeira dificuldade
-		MOV		DL, 14
-		JMP		SELECAO
-
-TROCA_ESQUERDA:
-		MOV	byte[cor], preto			;apaga seta		
-		CALL cursor
-    	MOV AL, [BX+selecao]
-		CALL	caracter
-
-		CMP		DL, 20						; compara para saber se esta na primeira dificuldade selecionavel
-		JL		volta_esq
-		SUB		DL, 20						; pula pra dificuldade anterior
-		JMP		SELECAO
-
-volta_esq:	
-		MOV		DL, 54						; da a volta pela direita para a ultima dificuldade
-		JMP		SELECAO
-
-;inicia o jogo, a dificuldade depende do valor de DL
-JOGO:		
-		;cria linha delimitadora inferior 
-		XOR 	AX, AX
-		MOV 	AX, 40
-		PUSH 	AX
-		MOV 	AX, 40
-		PUSH 	AX
-		MOV 	AX, 600
-		PUSH 	AX
-		MOV 	AX, 40
-		PUSH 	AX
-		CALL 	line
-
-		;cria linha delimitadora superior
-		MOV 	AX, 40
-		PUSH 	AX
-		MOV 	AX, 440
-		PUSH 	AX
-		MOV 	AX, 600
-		PUSH 	AX
-		MOV 	AX, 440
-		PUSH 	AX
-		CALL 	line
-
-		;apaga a seleçao de dificuldade
-		MOV		byte [cor], preto
-		;passa x1, y1 e x2, y2
-		MOV 	AX, 40
-		PUSH 	AX
-		MOV 	AX, 41
-		PUSH	AX
-		MOV		AX, 600
-		PUSH	AX
-		MOV		AX, 439
-    	PUSH    AX
-		CALL 	RETANGULO
-
-    ;desenha os blocos dos jogadores
-    	CALL 	DESENHA_BLOCOS_P1_E_P2
-
-TESTE2:	MOV		AX, [verifica1]
-		CMP		AX, [verifica2]
-		JE TESTE2
+		JE ESPERA
 		INC     WORD [verifica2]
 		AND     WORD [verifica2],7
 		;aqui serão feitas as comparaçoes para a movimentaçao, pausa e saida do jogo
 		MOV		AL, [1+tecla]
 		MOV		[tecla_primida], AL
 
-		CMP		byte[tecla_primida], 19h
-		JE		PAUSE
-		;verifica se a tecla de saida foi primida e sai caso sim
-		CMP 	byte[tecla_primida], 10h
-		JE 		FIM
-		JMP		TESTE2
-FIM:
-;sai do modo de video
-		CLI								
-        XOR     AX, AX					
-        MOV     ES, AX					
-        MOV     AX, [save_segment]			
-        MOV     [ES:INTr*4+2], AX		
-        MOV     AX, [save_offset]
-        MOV     [ES:INTr*4], AX
-		STI
-	 	MOV AH, 0   						; set video mode
-	 	MOV AL, [modo_anterior]   		; modo anterior
-	  	INT 10h
-		MOV AX, 4c00h
-		INT 21h
-		
-PAUSE:
-		MOV		AX, [verifica1]
-		CMP		AX, [verifica2]
-		JE PAUSE
-		INC     WORD [verifica2]
-		AND     WORD [verifica2],7
-		MOV		AL, [1+tecla]
-		MOV		[tecla_primida], AL
-		CMP		byte[tecla_primida], 19h
-		JE		TESTE2
-		JMP		PAUSE
-
-INTERRUPCAO_TECLADO:
-		PUSHF
-		PUSH	AX	
-		PUSH	BX	
-		PUSH	CX	
-		PUSH	DX	
-		PUSH	DS
-		PUSH	ES	
-
-		IN 		AL, 0x60
-		INC     WORD [verifica1]
-		AND     WORD [verifica1], 7
-		;MOV		BX, [verifica1]
-		MOV		[1+tecla], AL
-
-		; Limpa a interrupção do teclado
-		IN      AL, kb_ctl				
-        OR      AL, 80h					
-        OUT     kb_ctl, AL				
-        AND     AL, 7Fh					
-        OUT     kb_ctl, AL	
-		
-		; notifica fim da interrupçao ao PIC			
-        MOV     AL, eoi					
-        OUT     pictrl, AL
-		
-		POP		ES
-		POP		DS
-		POP 	DX
-		POP 	CX
-		POP 	BX
-		POP 	AX
+		POP DX
+		POP CX
+		POP BX
+		POP AX
 		POPF
-		IRET
+
+		RET
 
 ;*******************************************************************
 
